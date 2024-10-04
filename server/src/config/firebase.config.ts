e37b57dfdd2a4e601dd2562116eed96d3854e93c
BitFux <99053098+DerDunkleFux@users.noreply.github.com>
2024-10-04 18:55:40 -0500

    updated security

--- server/src/config/firebase.config.ts
+++ server/src/config/firebase.config.ts
@@ -2,21 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '/etc/secrets/.env' });
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: "AIzaSyDSIpSKIF5YCczqHqkdGywEN3VlWIt__m0",
-  authDomain: "mapemtest-69420.firebaseapp.com",
-  databaseURL: "https://mapemtest-69420-default-rtdb.firebaseio.com",
-  projectId: "mapemtest-69420",
-  storageBucket: "mapemtest-69420.appspot.com",
-  messagingSenderId: "34257012133",
-  appId: "1:34257012133:web:b3199cc38a10701d4c144c",
-  measurementId: "G-HSD67XT1YF"
-};
+export const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
